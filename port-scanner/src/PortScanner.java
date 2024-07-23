a338fb35c829b46b426e20bcad0f5602673b141e
Daria Arias <Daria Arias@DESKTOP-RRBKFBT>
2024-07-23 13:24:03 +0200

    Added fixed thread pool

--- port-scanner/src/PortScanner.java
+++ port-scanner/src/PortScanner.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PortScanner {
 	
@@ -10,17 +13,28 @@ public class PortScanner {
 	private final static int MAX_PORT_NUMBER = 65335;
 
 	public static void main(String[] args) {
-		scan("old-orel-city.narod.ru");
-
+		scan("en.wikipedia.org");
 	}
 
 	private static void scan(String host) {
-		for(int port = MIN_PORT_NUMBER; port<=MAX_PORT_NUMBER; port++) {
-			var inetSocketAddress = new InetSocketAddress(host, port);
-			try (var socket = new Socket()) {
-				socket.connect(inetSocketAddress, TIMEOUT);
-				System.out.printf("Host: %s, port %d is open\n", host, port);
-			} catch (IOException ignored) {}
+		ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
+		
+		for(int i = MIN_PORT_NUMBER; i<=MAX_PORT_NUMBER; i++) {
+			final int port = i;
+			executorService.execute(() -> {
+				var inetSocketAddress = new InetSocketAddress(host, port);
+				try (var socket = new Socket()) {
+					socket.connect(inetSocketAddress, TIMEOUT);
+					System.out.printf("Host: %s, port %d is open\n", host, port);
+				} catch (IOException ignored) {}
+			});
 		}
+		executorService.shutdown();
+		try {
+			executorService.awaitTermination(5, TimeUnit.MINUTES);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("Finish!");
 	}
 }
